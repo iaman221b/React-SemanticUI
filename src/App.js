67b0f12574efeaa4c1785f5f0d3caafd5e893132
Aman Tandon <iaman221b@gmail.com>
2019-09-28 01:00:05 +0530

    login

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Login from './login.js'
+import {Container} from 'semantic-ui-react'
+
+const App = () => (
+  <Container>
+    <Login />
+  </Container>
+);
+export default App;
+
+
